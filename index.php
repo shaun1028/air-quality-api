dcd509f3e8aefad280b40294815dc756a3f30141
shaun1028 <shaunooisy@gmail.com>
2026-08-15 21:20:38 +0800

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$file = __DIR__ . $path;
-
-if ($path !== '/' && file_exists($file) && !is_dir($file)) {
-    return false;
-}
-
-include __DIR__ . '/index.html';
